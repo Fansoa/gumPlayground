c498efe64d68b139822bb3ac39fc8d4c884dff7f
fansoa <fverin.contactpro@gmail.com>
2025-02-10 16:29:05 +0100

    add getMicAndCamera function to get micro and camera stream

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 console.log('Scripts file works !')
+
+const constraints = {
+    video: true,
+    audio: true
+}
+
+let stream = null;
+
+async function getMicAndCamera(e) {
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      /* use the stream */
+      
+    } catch (err) {
+      /* handle the error */
+    }
+}
+
+document.querySelector("#share").addEventListener('click', e => getMicAndCamera(e))
