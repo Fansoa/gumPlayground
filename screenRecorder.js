56b392c3693ab02500e1129af4adf52da66e75c9
fansoa <fverin.contactpro@gmail.com>
2025-02-11 11:51:32 +0100

    added functions related to media recording

---
+++ screenRecorder.js
@@ -0,0 +1,28 @@
+let recordedBlobs;
+let mediaRecorder;
+
+function startRecording() {
+  console.log("start recording");
+  mediaRecorder = new MediaRecorder(stream)
+  recordedBlobs = []
+  mediaRecorder.ondataavailable = e => {
+    console.log("🚀 ~ startRecording ~ e:", e)
+    recordedBlobs.push(e.data)
+    console.log("Data is available for the media recorder")
+  }
+  mediaRecorder.start()
+}
+
+function stopRecording() {
+  mediaRecorder.stop()
+  console.log("stop recorder");
+}
+
+function playRecording() {
+  console.log("play recording");
+  console.log(recordedBlobs)
+  const superBuffer = new Blob(recordedBlobs)
+  const recordVideoEl = document.querySelector("#other-video")
+  recordVideoEl.src = window.URL.createObjectURL(superBuffer)
+  recordVideoEl.play()
+}
